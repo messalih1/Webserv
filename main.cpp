cdb9ef1a35d74c326ea95f8bc72881e3475b11f3
messalih1 <mustaphaessalih1937@gmail.com>
2023-03-02 18:41:13 +0100

    try handling multi clients

--- main.cpp
+++ main.cpp
@@ -23,112 +23,70 @@ using std::endl;
 using std::vector;
 using std::stack;
 
-#include <iostream>
-#include <fstream>
 
-#include <iostream>
-#include <vector>
-#include <unistd.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <sys/event.h>
+#define PORT  8080
 
-#define MAX_EVENTS 32
 
-int main(int argc, char* argv[]) {
-    int server_socket, client_socket;
-    struct sockaddr_in server_addr, client_addr;
-    socklen_t client_addr_len = sizeof(client_addr);
-    int kq, nev, nread, nwrite;
-    struct kevent ev_set[MAX_EVENTS], ev_list[MAX_EVENTS];
+// • Checking the value of errno is strictly forbidden after a read or a write operation.
 
-    // Create socket and bind to a port
-    server_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_socket < 0) {
-        std::cerr << "Failed to create server socket" << std::endl;
-        return 1;
-    }
-
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(8080);
-    
-    if (bind(server_socket, (struct sockaddr*) &server_addr, sizeof(server_addr)) < 0) {
-        std::cerr << "Failed to bind to port" << std::endl;
-        return 1;
-    }
-
-    // Listen for incoming connections
-    if (listen(server_socket, SOMAXCONN) < 0) {
-        std::cerr << "Failed to listen for incoming connections" << std::endl;
-        return 1;
-    }
-
-    // Create kqueue instance
-    kq = kqueue();
-    
-    if (kq < 0) {
-        std::cerr << "Failed to create kqueue" << std::endl;
-        return 1;
-    }
-
-    
-    EV_SET(&ev_set[0], server_socket, EVFILT_READ, EV_ADD, 0, 0, NULL); // for monitiring the server
-    if (kevent(kq, ev_set, 1, NULL, 0, NULL) < 0) {
-        std::cerr << "Failed to add server socket to kqueue" << std::endl;
-        return 1;
-    }
-    while (true) {
-        // Wait for events
-        nev = kevent(kq, NULL, 0, ev_list, MAX_EVENTS, NULL);
-        if (nev < 0) {
-            std::cerr << "Error in kevent()" << std::endl;
-            continue;
-        }
-
-        for (int i = 0; i < nev; i++) 
-        {
-            if (ev_list[i].ident == server_socket) {
-                // New connection detected
-                client_socket = accept(server_socket, (struct sockaddr*) &client_addr, &client_addr_len);
-                if (client_socket < 0) {
-                    std::cerr << "Failed to accept connection from client\n";
-                    continue;
-                }
-
-                // Add the client socket to the kqueue
-                struct kevent kev;
-                EV_SET(&kev, client_socket, EVFILT_READ, EV_ADD, 0, 0, NULL);
-                if (kevent(kq, &kev, 1, NULL, 0, NULL) == -1) {
-                    std::cerr << "Failed to add client socket to kqueue\n";
-                    close(client_socket);
-                    continue;
-                }
-
-                std::cout << "New client connected\n";
-            } 
-            else 
-            {
-                // Handle data from client
-                int client_socket = ev_list[i].ident;
-                char buffer[1024];
-                int nbytes = recv(client_socket, buffer, sizeof(buffer), 0);
-                if (nbytes <= 0) {
-                    // Client closed connection or error occurred
-                    std::cout << "Client disconnected\n";
-                    EV_SET(&kev, client_socket, EVFILT_READ, EV_DELETE, 0, 0, NULL);
-                    kevent(kq, &kev, 1, NULL, 0, NULL);
-                    close(client_socket);
-                    continue;
-                }
-
-                // Process the data received from the client
-                std::cout << "Received data from client: " << buffer << "\n";
-
-                // Echo the data back to the client
-                send(client_socket, buffer, nbytes, 0);
-            }
+void error(const char * msg)
+{
+    perror(msg);
+    exit(0);
 }
+
+int main(int ac, char *argv[])
+{
+    int fd_server, new_socket;
+    long read_from_new_socket;
+    socklen_t len;
+
+    struct sockaddr_in serv_struct;// empty struct and will fill them by infos of socket
+
+    // create socket
+    fd_server = socket(AF_INET,SOCK_STREAM,0);
+    if(fd_server < 0)
+        error("error in opening socket");
+    
+    // set all bytes of the structure to 0, we ensure that any padding bytes are also set to 0,
+    // and that there are no uninitialized bytes in the structure.
+
+    //     first arg is pointer
+    std::memset(&serv_struct, 0, sizeof(serv_struct));
+
+    serv_struct.sin_family = AF_INET; // ivp4
+    serv_struct.sin_addr.s_addr = INADDR_ANY; // any ip can send to this server 
+    serv_struct.sin_port = htons(PORT);
+
+    // bind the socket to the server address
+    /*
+    assigning a specific IP address and port number to the socket created by 
+    the server, so that the server can start listening for incoming connections from clients. 
+    */
+
+    if (bind(fd_server,  reinterpret_cast<struct sockaddr *> (&serv_struct), sizeof(serv_struct)) < 0) 
+        error("ERROR on binding");
+
+    if (listen(fd_server, 10) < 0)
+        perror("In listen()");
+    
+    len = sizeof(serv_struct);
+    
+    while(1)
+    {
+        cout << "\n+++++++ Waiting for new connection ++++++++\n\n";
+        if ((new_socket = accept(fd_server, reinterpret_cast<struct sockaddr *> (&serv_struct), &len) ) < 0 )
+            perror("In accept()");
+        
+        char buffer[30000] = {0};
+        read_from_new_socket = read( new_socket , buffer, 30000);
+        cout << buffer << endl;
+        
+        close(new_socket);
+         
+        // break;
     }
+
+    // use  setsockopt
+}
+ 
--- server.cpp
+++ server.cpp
@@ -26,7 +26,7 @@ using std::vector;
 using std::stack;
 
 
-#define PORT  8881
+#define PORT  8080
 #define MAX_EVENTS 10
 #define BUFFER_SIZE 1024
 
@@ -60,7 +60,7 @@ int main(int ac, char *argv[])
     serv_struct.sin_addr.s_addr = INADDR_ANY;
     serv_struct.sin_port = htons(PORT);
 
-
+    optval = 1;
     if(setsockopt(fd_server, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)) < 0)
         error("in setsockopt()");
 
@@ -71,56 +71,67 @@ int main(int ac, char *argv[])
     if (listen(fd_server, 10) < 0)
         perror("In listen()");
     
-
     len = sizeof(serv_struct);
     
     kq = kqueue();
 
     EV_SET(&ev_serv_set[0], fd_server, EVFILT_READ, EV_ADD, 0, 0, NULL);
     
-    if ((kevent(kq, ev_serv_set, 1, NULL, 0, NULL)) < 0)
+    if ((kevent(kq, ev_serv_set, 1, NULL, 0, NULL)) < 0)// register the server socket for read events
         error("in kevent()");
     
-    while(1)
+    struct kevent kev;
+    while(1)// allows the program to continuously monitor for events on the registered file descriptors.
     {
-        nevents = kevent(kq, NULL, 0, ev_clt_set, MAX_EVENTS, NULL);
-        if (nevents < 0) {
-            std::cerr << "Error in kevent()" << std::endl;
-            continue;
-        }
-       
-        for (int i = 0; i < nevents; i++) 
+        nevents = kevent(kq, NULL, 0, ev_clt_set, MAX_EVENTS, NULL);// waiting for  server && client events
+        if (nevents < 0) 
+            error("in kevent()");
+        if(nevents)
         {
-            if(ev_clt_set[i].ident == fd_server)// if event occur in server
+            // cout << nevents << endl;
+            for (int i = 0; i < nevents; i++) // loop for numbers of events occurs
             {
-                if ((new_socket = accept(fd_server, reinterpret_cast<struct sockaddr *> (&serv_struct), &len) ) < 0 )
-                    perror("In accept()");
+                if(ev_clt_set[i].ident == fd_server)// if new connection
+                {
+                    if ((new_socket = accept(fd_server, reinterpret_cast<struct sockaddr *> (&serv_struct), &len) ) < 0 )// will accept it
+                        perror("In accept()");
+                    
+                    // Add the client socket to the kqueue
+                    EV_SET(&kev, new_socket, EVFILT_READ, EV_ADD, 0, 0, NULL);// prepar new connection , new clinet for incomming data
+                    if (kevent(kq, &kev, 1, NULL, 0, NULL) == -1) // add new connection to kevents, 
+                        error("in kevent()");
+                    else
+                        std::cout << "New client connected\n";
+                }
+                else 
+                { 
+                    // if incomming data will enter the first if because it is not new connection , will enter here and Handle data from client
+                    int client_socket = ev_clt_set[i].ident;// have fd of the client
+                
+                    int nbytes = recv(client_socket, buffer, sizeof(buffer), 0);
+                    if (nbytes == 0) 
+                    {
+                        // Client closed connection or error occurred
+                        std::cout << "Client disconnected\n";
+                        EV_SET(&kev, client_socket, EVFILT_READ, EV_DELETE, 0, 0, NULL);//In the example code you provided, EV_SET is used to modify an existing event, specifically to delete it from the kqueue.
+                        kevent(kq, &kev, 1, NULL, 0, NULL); // to  stop monitoring the client socket for incoming data
+                        close(client_socket);
+                    }
+                    else
+                    {
+                        std::cout << "Received data from client: " << buffer << "\n";
+                        // send(client_socket, buffer, nbytes, 0);
+                    }
+                }
             }
-            // Add the client socket to the kqueue
-            struct kevent kev;
-            EV_SET(&kev, new_socket, EVFILT_READ, EV_ADD, 0, 0, NULL);
-            if (kevent(kq, &kev, 1, NULL, 0, NULL) == -1) {
-                std::cerr << "Failed to add client socket to kqueue\n";
-                close(new_socket);
-                continue;
-            }
-            std::cout << "New client connected\n";
+
         }
-
-        
-
+    }
+}
         
         // char buffer[30000] = {0};
         // read_from_new_socket = read( new_socket , buffer, 30000);
         // cout << buffer << endl;
-        
-        close(new_socket);
-         
-        // break;
-    }
-
-    // use  setsockopt
-}
     
         // cout << "\n+++++++ Waiting for new connection ++++++++\n\n";
  
